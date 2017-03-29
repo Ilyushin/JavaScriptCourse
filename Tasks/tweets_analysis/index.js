0dab18d6242b18c6e398a396976fdf82eda831aa
Ilyushin Evgeniy <john.ilyushin@gmail.com>
2017-03-29 13:47:52 +0300

    Added prerequisites.

--- Tasks/tweets_analysis/index.js
+++ Tasks/tweets_analysis/index.js
@@ -1,5 +1,9 @@
+'use strict';
+
 const LineByLineReader = require('line-by-line'),
     lr = new LineByLineReader('./input/dataSet.csv');
+
+const data = [];
 
 lr.on('error', function (err) {
     // 'err' contains error object
@@ -12,10 +16,16 @@ lr.on('line', function (line) {
     // ...do your asynchronous line processing..
     setTimeout(function () {
         // ...and continue emitting lines.
+        data.push(line);
         lr.resume();
     }, 100);
 });
 
 lr.on('end', function () {
     // All lines are read, file is closed now.
+    main();
 });
+
+function main () {
+
+}
