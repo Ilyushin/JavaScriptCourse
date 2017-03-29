45d9a301ea410b39e00df6bb9248e627c70306d2
Ilyushin Evgeniy <john.ilyushin@gmail.com>
2017-03-29 13:13:29 +0300

    Added a template for reading the dataSet.

--- Tasks/tweets_analysis/index.js
+++ Tasks/tweets_analysis/index.js
@@ -1,3 +1,21 @@
-/**
- * Created by evgeniy on 3/29/17.
- */
+const LineByLineReader = require('line-by-line'),
+    lr = new LineByLineReader('./input/dataSet.csv');
+
+lr.on('error', function (err) {
+    // 'err' contains error object
+});
+
+lr.on('line', function (line) {
+    // pause emitting of lines...
+    lr.pause();
+
+    // ...do your asynchronous line processing..
+    setTimeout(function () {
+        // ...and continue emitting lines.
+        lr.resume();
+    }, 100);
+});
+
+lr.on('end', function () {
+    // All lines are read, file is closed now.
+});
